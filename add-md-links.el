d2cb0ed83df21630060550160cb3629645e27046
Noboru Ota <me@nobiot.com>
2020-06-05 09:12:50 +0200

    Add 10000-org-linked-files

--- add-md-links.el
+++ add-md-links.el
@@ -1,21 +1,21 @@
+;; This is a very rudimentry script to batch process adding links to
+;; org files.
+
 ;; Use
 
 ;; 1. Change the variables
 ;;    - Limit for links
 ;;    - Number of notes in the directory
 ;;    - The path to the directory
+;;    - file name extension
 ;; 2. M-x load-file [this file]
-;; 3. Loading will automatically start dolist!
-;;    so careful before you do so.
+;; 3. Loading will automatically start dolist.
+;;    Undo is not considered so careful before
+;;    you load this file.
 
-;; For each note, rendomly pick n
-;; Repeat n times for a file
-;; Randomly pick a note from N
-;; Add links to m notes to a file
-
-(setq limit-for-links 10)
-(setq number-of-notes 100)
-(setq directory "./100-markdown-files")
+(setq limit-for-links 500)
+(setq number-of-notes 10000)
+(setq directory "./10000-markdown-files")
 (setq file-ext ".org")
 
 (defun generate-link-string()
@@ -32,6 +32,6 @@
     (insert "\n\n")
     (write-region (point-min) (point-max) file)))
 
-(dolist (path (directory-files "./100-markdown-files" t file-ext nil))
+(dolist (path (directory-files directory t file-ext nil))
   (let ((number-of-links (random limit-for-links)))
     (add-links-n-times-to-file number-of-links path)))
